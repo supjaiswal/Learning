d44e33dc65218c24b7dc65daa132bf82ad20798d
Supriya Jaiswal <Supriya Jaiswal@192.168.0.34>
2020-07-07 16:01:17 +0530

    second commit

---
+++ learning/src/learning/hello2.java
@@ -0,0 +1,14 @@
+
+package learning;
+
+import java.util.*;
+public class hello2 {
+	public static void main(String[] args) {
+	Scanner userInputScanner= new Scanner(System.in);
+	 System.out.print("what is your name");
+	 String userInputName=userInputScanner.nextLine();
+	 System.out.println("Hello"+ userInputName+"!");
+	 
+
+	}
+}	
