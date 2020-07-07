b51ec226af88ebd2b6c4677943ecdeb2083578d3
Supriya Jaiswal <Supriya Jaiswal@192.168.0.34>
2020-07-07 16:02:50 +0530

    variable commit

---
+++ learning/src/learning/variable.java
@@ -0,0 +1,15 @@
+package learning;
+
+public class variable {
+	static int staticVariable=1;
+	int instanseVariable=2;
+	public void methodName(int methodParameter) {
+		int methodLocalVariable=3; 
+		
+		if(true) {
+			int blockVariable=4;
+		}
+	
+	}
+
+}
